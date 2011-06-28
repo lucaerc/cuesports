6b722253fc9048506e7a6528b3d6fc380f6d0896
Federico D'Ambrosio <federico.dambrosio@gmail.com>
2011-06-28 12:04:36 +0200

    Correzioni, prima documentazione di Stick, modifiche rispetto alla definizione originaria
    Documentazione di Stick, correzione di _J con _F (altrimenti a che serve il tau?), aggiunto _didsend per notificare l'avvenuto colpo, correzioni che ieri notte c'avevo sonno e ho fatto cagate. Andrà modificato una volta implementato Impulse e determinato tau.

--- Stick.cc
+++ Stick.cc
@@ -1,19 +1,26 @@
 #include "Stick.hh"
 #include <cmath>
 
-Stick::Stick(double J, double angle){
-    _J=J;
+Stick::Stick(double F, double angle){
+    _F=F;
     _angle=angle;
+    _tau=0.;//valore di tau
+    _didsend=false;
 }
 
-void hit(Ball & ball){
-    double Jx=J*sin(angle);
-    double Jy=J*cos(angle);
+void Stick::hit(Ball & ball){
+    double Jx=F*sin(angle)*_tau; //siccome l'angolo è lungo la verticale, il seno restituisce x e il coseno y
+    double Jy=F*cos(angle)*_tau;
     
     Impulse J(Jx,Jy); //questo andrà modificato quando avrai definito Impulse
     
-    other.ball(J); //trasmette l'impulso alla palla ball
+    ball.hurt(J); //trasmette l'impulso alla palla ball
+    
+    _didsend=true;
     
 }
     
+bool Stick::didsend(){
+    return _didsend;
+}
 
--- Stick.hh
+++ Stick.hh
@@ -1,12 +1,17 @@
 
 class Stick {
-double _J; //fornito dall'utente
-double _angle; //fornito dall'utente
-double _tau; //è una costante
+    
+    double _F; //fornito dall'utente
+    double _angle; //fornito dall'utente
+    double _tau; //e' una costante
+    bool _didhit; //ha gia' mandato il colpo?
 
 public:
-Stick(double J, double angle);
-~Stick();
-
-void hit(Ball & ball);
+    
+    Stick(double F, double angle);
+    ~Stick();
+    void hit(Ball & ball);
+    bool didsend();
+    
+    
 };
